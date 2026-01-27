afa28a295b5dec61d9c6a013b9782fc33955b960
Bloorize <163452008+Bloorize@users.noreply.github.com>
2026-01-27 16:32:55 -0700

    Configure GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import DisplayView from './DisplayView'
 import AdminView from './AdminView'
 import './index.css'
